fc1c2cc85b49e10c1ca67ce3f17f6e9833244aac
Chamod-alt <152728704+Chamod-alt@users.noreply.github.com>
2025-06-27 23:56:59 +0530

    delea a file and update final

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 
-
+//Importing Firebase Functions
 import { initializeApp } from 'firebase/app';
 import {
   getDatabase,
@@ -17,7 +17,7 @@ import {
   onChildRemoved,
 } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-
+// Firebase Project Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBxd778T6RPeeiYO-zoEd522qrGUIMZnY4",
     authDomain: "chatbot-64275.firebaseapp.com",
@@ -27,9 +27,10 @@ const firebaseConfig = {
     appId: "1:970800393996:web:ac1f2d776724001d21e329",
     measurementId: "G-42G4KN8ZJ6"
 };
-
+// Initializing Firebase App
 const app = initializeApp(firebaseConfig);
-
+// Exporting Services
 export const db = getDatabase(app);
 export const auth = getAuth(app);
+// Exporting Realtime Database Utilities
 export { ref, push, onValue, onChildAdded, get, query, limitToLast, remove, update, child,onChildChanged, onChildRemoved };
